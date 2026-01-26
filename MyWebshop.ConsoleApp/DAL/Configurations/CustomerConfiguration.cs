98d83b401e7cc7543461a471b89113fa7baf6a6c
Kees van Loenen <kees.vanloenen@infosupport.com>
2026-01-26 15:10:31 +0100

    Moved one-to-many configuration to Orders entity

--- MyWebshop.ConsoleApp/DAL/Configurations/CustomerConfiguration.cs
+++ MyWebshop.ConsoleApp/DAL/Configurations/CustomerConfiguration.cs
@@ -28,10 +28,5 @@ public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
             // .ValueGeneratedOnAdd()      // niet nodig hier, .HasDefaultValueSql impliceert het al
             .IsRequired();
 
-
-        builder.HasMany(c => c.Orders)
-            .WithOne(o => o.Customer)
-            .HasForeignKey(o => o.CustomerId)
-            .OnDelete(DeleteBehavior.Cascade);
     }
 }
---
+++ MyWebshop.ConsoleApp/DAL/Configurations/OrderConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyWebshop.ConsoleApp.Models;
+
+namespace MyWebshop.ConsoleApp.DAL.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        builder.HasKey(o => o.Id);
+
+        builder.Property(o => o.TotalAmount)
+            .HasColumnType("decimal(18,2)")
+            .HasPrecision(18, 2);
+
+        builder.HasOne(o => o.Customer)
+            .WithMany(c => c.Orders)
+            .HasForeignKey(o => o.CustomerId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
