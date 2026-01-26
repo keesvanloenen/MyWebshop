f95ac1c7a30d7d6b45687e82bbab82c8229f42d6
Kees van Loenen <kees.vanloenen@infosupport.com>
2026-01-26 14:13:36 +0100

    Reordered methods for finability

--- MyWebshop.ConsoleApp/Program.cs
+++ MyWebshop.ConsoleApp/Program.cs
@@ -11,7 +11,6 @@ internal class Program
     static void Main(string[] args)
     {
         Console.OutputEncoding = Encoding.UTF8;
-
         var options = BuildContextOptions();
 
         InitializeDb(options);
@@ -22,24 +21,53 @@ internal class Program
         ShowProductsMichelAlternative(options);
     }
 
-    private static void ShowProductsMichelAlternative(DbContextOptions<MyWebshopDbContext> options)
+
+    private static DbContextOptions<MyWebshopDbContext> BuildContextOptions()
     {
-        Console.WriteLine("\n=== Alternatief Michel ===");
+        var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+        var config = builder.Build();
+
+        return new DbContextOptionsBuilder<MyWebshopDbContext>()
+            .UseSqlServer(config.GetConnectionString("DefaultConn"))
+            .Options;
+    }
+
+    private static void InitializeDb(DbContextOptions<MyWebshopDbContext> options)
+    {
+        using var context = new MyWebshopDbContext(options);
+        context.Database.EnsureDeleted();
+        context.Database.EnsureCreated();
+    }
+
+    private static void DataSeed(DbContextOptions<MyWebshopDbContext> options)
+    {
         using var context = new MyWebshopDbContext(options);
 
-        var physicalProducts = context.Products.OfType<PhysicalProduct>();
-        var digitalProducts = context.Products.OfType<DigitalProduct>();
+        var customer1 = new Customer { Name = "Ab" };
+        var customer2 = new Customer { Name = "Bo" };
+        var customer3 = new Customer { Name = "Cas" };
 
-        Console.WriteLine("\nPhysical Products:");
-        foreach (var p in physicalProducts)
-        {
-            Console.WriteLine($"[{p.Id}] {p.Name} - {p.Price:C} | Weight = {p.Weight}");
-        }
+        context.Customers.AddRange([customer1, customer2, customer3]);
+        context.SaveChanges();
 
-        Console.WriteLine("\nDigital Products:");
-        foreach (var d in digitalProducts)
+        var physicalProduct1 = new PhysicalProduct { Name = "Laptop", Price = 999.99m, Weight = 1.5m };
+        var physicalProduct2 = new PhysicalProduct { Name = "Mouse", Price = 19.99m, Weight = 0.1m };
+        var digitalProduct1 = new DigitalProduct { Name = "C# for Dummies", Price = 9.99m, FileSizeInMB = 5 };
+        var digitalProduct2 = new DigitalProduct { Name = "LINQ Course", Price = 49.99m, FileSizeInMB = 1200 };
+
+        context.Products.AddRange([physicalProduct1, physicalProduct2, digitalProduct1, digitalProduct2]);
+        context.SaveChanges();
+    }
+
+    private static void ShowCustomers(DbContextOptions<MyWebshopDbContext> options)
+    {
+        using var context = new MyWebshopDbContext(options);
+
+        var customers = context.Customers;
+
+        foreach (var customer in customers) 
         {
-            Console.WriteLine($"[{d.Id}] {d.Name} - {d.Price:C} | Size = {d.FileSizeInMB} MB");
+            Console.WriteLine($"[{customer.Id}] {customer.Name}");
         }
     }
 
@@ -71,54 +99,24 @@ internal class Program
         }
     }
 
-
-
-    private static DbContextOptions<MyWebshopDbContext> BuildContextOptions()
+    private static void ShowProductsMichelAlternative(DbContextOptions<MyWebshopDbContext> options)
     {
-        var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
-        var config = builder.Build();
-
-        return new DbContextOptionsBuilder<MyWebshopDbContext>()
-            .UseSqlServer(config.GetConnectionString("DefaultConn"))
-            .Options;
-    }
-
-    private static void DataSeed(DbContextOptions<MyWebshopDbContext> options)
-    {
+        Console.WriteLine("\n=== Alternatief Michel ===");
         using var context = new MyWebshopDbContext(options);
 
-        var customer1 = new Customer { Name = "Ab" };
-        var customer2 = new Customer { Name = "Bo" };
-        var customer3 = new Customer { Name = "Cas" };
+        var physicalProducts = context.Products.OfType<PhysicalProduct>();
+        var digitalProducts = context.Products.OfType<DigitalProduct>();
 
-        context.Customers.AddRange([customer1, customer2, customer3]);
-        context.SaveChanges();
-
-        var physicalProduct1 = new PhysicalProduct { Name = "Laptop", Price = 999.99m, Weight = 1.5m };
-        var physicalProduct2 = new PhysicalProduct { Name = "Mouse", Price = 19.99m, Weight = 0.1m };
-        var digitalProduct1 = new DigitalProduct { Name = "C# for Dummies", Price = 9.99m, FileSizeInMB = 5 };
-        var digitalProduct2 = new DigitalProduct { Name = "LINQ Course", Price = 49.99m, FileSizeInMB = 1200 };
-
-        context.Products.AddRange([physicalProduct1, physicalProduct2, digitalProduct1, digitalProduct2]);
-        context.SaveChanges();
-    }
-
-    private static void InitializeDb(DbContextOptions<MyWebshopDbContext> options)
-    {
-        using var context = new MyWebshopDbContext(options);
-        context.Database.EnsureDeleted();
-        context.Database.EnsureCreated();
-    }
-
-    private static void ShowCustomers(DbContextOptions<MyWebshopDbContext> options)
-    {
-        using var context = new MyWebshopDbContext(options);
-
-        var customers = context.Customers;
-
-        foreach (var customer in customers) 
+        Console.WriteLine("\nPhysical Products:");
+        foreach (var p in physicalProducts)
         {
-            Console.WriteLine($"[{customer.Id}] {customer.Name}");
+            Console.WriteLine($"[{p.Id}] {p.Name} - {p.Price:C} | Weight = {p.Weight}");
+        }
+
+        Console.WriteLine("\nDigital Products:");
+        foreach (var d in digitalProducts)
+        {
+            Console.WriteLine($"[{d.Id}] {d.Name} - {d.Price:C} | Size = {d.FileSizeInMB} MB");
         }
     }
 }
